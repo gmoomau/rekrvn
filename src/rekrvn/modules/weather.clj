545185d993b35300791b3ca68a32372e108e2e8f
George Moomau <tallsbyxiv@gmail.com>
2012-07-10 00:03:07 -0400

    Fix weather api regex to use correct time

--- src/rekrvn/modules/weather.clj
+++ src/rekrvn/modules/weather.clj
@@ -1,15 +1,14 @@
 (ns rekrvn.modules.weather
   (:require [rekrvn.hub :as hub])
   (:require [http.async.client :as c])
-  (:use [http.async.client.request :only [url-encode]])
-  )
+  (:use [http.async.client.request :only [url-encode]]))
 
 (def mod-name "weather")
 
 (defn niceify [raw-weather]
-  (let [regx #".+\|(\d+)\|.+\|.+\|(.+)\|.+\|.+\|(.+)\|(.+)\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|(.+)\|(.+)\|(\d\d:\d\d \S+) \((.+)\)\|.+\|.+\|.+\|"
-        [temp humidity pressure sky sloc bloc clock zone] (rest (re-find regx raw-weather))]
-    (str sloc ", " bloc " (" clock " " zone "): " temp "°F - " sky " | "
+  (let [regx #"(\d\d:\d\d \S\S \S\S\S).+\|(\d+)\|.+\|.+\|(.+)\|.+\|.+\|(.+)\|(.+)\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|.+\|(.+)\|(.+)\|.+\|.+\|.+\|.+\|"
+        [clock temp humidity pressure sky sloc bloc] (rest (re-find regx raw-weather))]
+    (str sloc ", " bloc " (" clock "): " temp "°F - " sky " | "
          humidity " Humidity | Barometer: " pressure)))
 
 
@@ -23,6 +22,6 @@
 (defn weather [[location] reply]
   (if-let [raw-weather (api-lookup location)]
     (reply mod-name (niceify raw-weather))
-    (reply mod-name "For some reason this is only works if you give it a zip code.")))
+    (reply mod-name "For some reason this only works if you give it a zip code.")))
 
 (hub/addListener "weather" #"^irc.*PRIVMSG \S+ :\.w (.+)$" weather)
