ed8720e261b5d44c48f356db0976cd050d5f7e30
George Moomau <georgemoomau@gmail.com>
2012-12-08 18:24:15 -0500

    add a module to yell when people yell on irc
    - adds a mongodb requirement to the project
    - when someone yells in the channel, stores it to mongodb and responds
      with something that was yelled earlier in that channel

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "irc bot framework"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [http.async.client "0.4.3"]
-                 [cheshire "4.0.0"]]
+                 [cheshire "4.0.0"]
+                 [org.clojars.sids/htmlcleaner "2.1"]
+                 [com.novemberain/monger "1.4.1"]]
   :main rekrvn.hub)
---
+++ src/rekrvn/modules/caps.clj
@@ -0,0 +1,34 @@
+(ns rekrvn.modules.caps
+  (:refer-clojure :exclude [sort find])
+  (:require [rekrvn.hub :as hub]
+            [monger.core :as mg]
+            [monger.collection :as mc])
+  (:use monger.query)
+  (:import [com.mongodb MongoOptions ServerAddress]
+           [org.bson.types ObjectId]))
+
+(def modName "caps")
+
+(defn caps [[channel line] reply]
+  (do
+    (mg/connect!)
+    (mg/set-db! (mg/get-db "rekrvn"))
+    (let [new-doc {:_id (ObjectId.) :text line :channel channel}
+          ;; find a random caps sentence
+          ;; the (skip ...) is slow, but the alternative (adding a "random" field
+          ;; to the documents and choosing the doc with a "random" closest to
+          ;; some other random number) is not uniformly random
+          num-lines (mc/count "caps" {:channel channel})
+          result (with-collection "caps"
+                   (find {:channel channel})
+                   (skip (rand num-lines))
+                   (limit 1))]
+      (when (> (count line) 3)
+        (mc/insert "caps" new-doc))
+
+      (when-let [out-line (first result)]
+        ;; result is a lazy seq of docs
+        (reply modName (:text out-line)))
+      (mg/disconnect!))))
+
+(hub/addListener modName #"^irc.*PRIVMSG #(\S+) :([^a-z]*[A-Z]+[^a-z]*)$" caps)
