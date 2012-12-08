8f2b92fadb159df7b7bb618ca7d2adb37bc1c330
George Moomau <georgemoomau@gmail.com>
2012-12-08 18:31:27 -0500

    update TODO

--- src/rekrvn/modules/irc/module_controls.clj
+++
@@ -1,9 +0,0 @@
-;; WARNING: You probably shouldn't use .load on things with long-
-;; lived connections, like irc or twitter
-(ns rekrvn.irc.modcontrols
-  (:require [rekrvn.hub :as hub]))
-
-(hub/addListener
-  "irc.modcontrols"
-  #"^irc.*PRIVMSG \S+ :\.load (\S+)$"
-  (fn [[modName] reFn] (hub/reload modName)))
