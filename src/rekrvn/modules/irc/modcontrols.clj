b0a7ba27e3513cd0f0021682ada021b863b7b67b
George Moomau <georgemoomau@gmail.com>
2012-12-08 18:43:35 -0500

    change namespace for module-loading module

---
+++ src/rekrvn/modules/irc/modcontrols.clj
@@ -0,0 +1,8 @@
+;; WARNING: You probably shouldn't use .load on things with long-
+;; lived connections, like irc or twitter
+(ns rekrvn.modules.irc.modcontrols
+  (:require [rekrvn.hub :as hub]))
+
+(hub/addListener
+  #"^irc.*PRIVMSG \S+ :\.load (\S+)$"
+  (fn [[modName] reFn] (hub/reload modName)))
