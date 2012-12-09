cd46e4b35042037fce1caf34adeae7e1804a8a4e
George Moomau <georgemoomau@gmail.com>
2012-12-08 19:11:47 -0500

    minor style fix for hub.clj

--- src/rekrvn/hub.clj
+++ src/rekrvn/hub.clj
@@ -1,6 +1,5 @@
 (ns rekrvn.hub
-  (:require [rekrvn.config])
-  )
+  (:require [rekrvn.config]))
 
 (def listeners (ref [])) ;; list of triggers
 
@@ -13,26 +12,22 @@
       (actFn (rest results) reply)))))
 
 (defn addListener [modname matcher action]
-  (dosync (alter listeners conj {:mod modname :matcher matcher :action action}))
-  )
+  (dosync (alter listeners conj {:mod modname :matcher matcher :action action})))
 
 (defn modload [modId]
   (let [module (symbol (str "rekrvn.modules." modId))]
     (try
       (require module :reload)
-    (catch Exception e (str "Caught exception: " (.getMessage e)))))
-  )
+    (catch Exception e (str "Caught exception: " (.getMessage e))))))
 
 (defn reload [modId]
   (do
     (dosync (ref-set listeners (remove (fn [trig] (= modId (:mod trig))) @listeners)))
-    (modload modId))
-  )
+    (modload modId)))
 
 (defn initMods []
   (doseq [modName rekrvn.config/modules] (modload modName)))
 
 
 (defn -main [& args]
-  (initMods)
-  )
+  (initMods))
