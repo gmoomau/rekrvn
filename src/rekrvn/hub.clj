55cb4853942ad03c2543d37110ece2268994dd7e
Cole Brown <bigswim@gmail.com>
2013-02-18 23:29:03 -0500

    Attempt at filtering

--- src/rekrvn/hub.clj
+++ src/rekrvn/hub.clj
@@ -6,12 +6,14 @@
 ;; string content fn reply
 (defn broadcast
   ([content] (broadcast content (fn [& args])))
-  ([content reply]
-   (doseq [{matcher :matcher actFn :action} @listeners]
-     (doseq [results (re-seq matcher content)]
-       (try
-         (actFn (rest results) reply)
-         (catch Exception e (println (str "Caught exception: " (.getMessage e)))))))))
+  ([content reply] (broadcast content (fn [& args]) (fn [_] true)))
+  ([content reply filter-fn]
+   (doseq [{matcher :matcher act-fn :action mod-name :mod} @listeners]
+     (when (filter mod-name)
+       (doseq [results (re-seq matcher content)]
+         (try
+           (act-fn (rest results) reply)
+           (catch Exception e (println (str "Caught exception: " (.getMessage e))))))))))
 
 (defn addListener [modname matcher action]
   (dosync (alter listeners conj {:mod modname :matcher matcher :action action})))
