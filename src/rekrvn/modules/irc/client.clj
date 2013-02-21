780f91549af4d8eb95fa84a1541f766a7c7506c3
Cole Brown <bigswim@gmail.com>
2013-02-21 14:09:57 -0500

    Agent-ify IRC client

--- src/rekrvn/modules/irc/client.clj
+++ src/rekrvn/modules/irc/client.clj
@@ -26,9 +26,12 @@
     (alter conn assoc :exit true)))
 
 (defn write [conn msg]
-  (doto (:out @conn)
-    (.println (str msg "\r"))
-    (.flush)))
+  (send-off (:out @conn)
+    (fn [out]
+      (doto out
+        (.println (str msg "\r"))
+        (.flush))
+      out)))
 
 (defn raw [[server cmd] replyFn]
   (when-let [conn (get @connections server)]
@@ -76,15 +79,20 @@
 (defn connect [server]
   (let [socket (Socket. (:server server) (:port server))
         in (BufferedReader. (InputStreamReader. (.getInputStream socket)))
-        out (PrintWriter. (.getOutputStream socket))
+        out (agent (PrintWriter. (.getOutputStream socket)))
         conn (ref {:in in :out out :queue []})]
     (dosync (alter connections assoc (:network server) conn))
     (doto (Thread. #(conn-handler conn server)) (.start))
     conn))
 
+(defn wait-until-registered [server-msg] (fn [out in]
+  (loop [msg (.readLine in)]
+    (if (re-find (re-pattern (str server-msg " :")) msg)
+      out
+      (recur (.readLine in))))))
+
 (defn conn-handler [conn server]
   (let [serverMsg (str "^:\\S+ \\d\\d\\d " (:nick server))
-        registered (ref false)
         network (:network server)
         register (fn [nick]
                    (write conn (str "NICK " nick))
@@ -93,16 +101,17 @@
     ;; register with server
     (register (:nick server))
 
+    ;; block outgoing comms until i am registered
+    (let [my-conn @conn]
+      (send-off (:out my-conn) (wait-until-registered serverMsg) (:in my-conn)))
+
     (doseq [chan (:channels server)] (joinChan conn chan))
 
     ;; handle messages
     (while (not (:exit @conn))
-      (while (or (not @registered) (.ready (:in @conn)))
+      (while (.ready (:in @conn))
         (let [msg (.readLine (:in @conn))]
           (println "fromirc" msg)
-          (when (and (not @registered) (re-find (re-pattern (str serverMsg " :")) msg))
-            (dosync (ref-set registered (ref true))))
-
           ;; maintaining connection + internal irc state
           (when (re-find #"^ERROR :Closing Link:" msg)
             (quit conn))
@@ -110,46 +119,45 @@
           (when (re-find #"^PING" msg)
             (write conn (str "PONG " (re-find #":.*" msg))))
 
-          (when @registered
-            ;; ugly hacks below
-            (when-let [rawcmd (re-find #"^:grog\S+ PRIVMSG #dump :\.raw (\S+) (.*)" msg)]
-              (raw (rest rawcmd) (fn [& args])))
-            ;; slightly less ugly hacks below
-            (when (re-find #"127.0.0.1.*!quit" msg)
-              (quit conn))
+          ;; ugly hacks below
+          (when-let [rawcmd (re-find #"^:grog\S+ PRIVMSG #dump :\.raw (\S+) (.*)" msg)]
+            (raw (rest rawcmd) (fn [& args])))
+          ;; slightly less ugly hacks below
+          (when (re-find #"127.0.0.1.*!quit" msg)
+            (quit conn))
 
-            ;; channel management
-            (when-let [joined (re-find (re-pattern (str serverMsg " = (\\S+) :")) msg)]
-              (dosync (alter currentChannels conj (str network "#" (second joined)))))
-            (when-let [kicked (re-find #"\S+ KICK (#\S+) " msg)]
-              ;; current regex can be faked
-              ;; rejoin functionality goes here
-              (dosync (alter currentChannels disj (str network "#" (second kicked)))))
+          ;; channel management
+          (when-let [joined (re-find (re-pattern (str serverMsg " = (\\S+) :")) msg)]
+            (dosync (alter currentChannels conj (str network "#" (second joined)))))
+          (when-let [kicked (re-find #"\S+ KICK (#\S+) " msg)]
+            ;; current regex can be faked
+            ;; rejoin functionality goes here
+            (dosync (alter currentChannels disj (str network "#" (second kicked)))))
 
-            (when-let
-              [invited (re-find (re-pattern (str "INVITE " (:nick server) " :(#\\S+)$")) msg)]
-              (joinChan conn (second invited)))
+          (when-let
+            [invited (re-find (re-pattern (str "INVITE " (:nick server) " :(#\\S+)$")) msg)]
+            (joinChan conn (second invited)))
 
-            ; module management
-            (when-let [cmd (re-find #"PRIVMSG (\S+) :\.(en)?(dis)?able (\S+)$" msg)]
-              (let [[source enable disable module] (rest cmd)]
-                (cond
-                  enable (modAllow network source module)
-                  disable (modDeny network source module))))
+          ; module management
+          (when-let [cmd (re-find #"PRIVMSG (\S+) :\.(en)?(dis)?able (\S+)$" msg)]
+            (let [[source enable disable module] (rest cmd)]
+              (cond
+                enable (modAllow network source module)
+                disable (modDeny network source module))))
 
-            ;; irc stuff that happens. this should/will all be changed soon
-            (if-let [recip (re-find #"PRIVMSG (\S+) :" msg)]
-              ;; privmsgs have replyfns
-              (let [reply (fn [modName msg]
-                            (doSomething [modName network (second recip) msg] nil))
-                    filter-fn (fn [mod-name]
-                                (let [my-recip (str network "#" (second recip))]
-                                  (permits my-recip mod-name)))]
-                (hub/broadcast (str "irc " msg) reply filter-fn))
-              ;; otherwise don't
-              (hub/broadcast (str "irc " msg))))))
+          ;; irc stuff that happens. this should/will all be changed soon
+          (if-let [recip (re-find #"PRIVMSG (\S+) :" msg)]
+            ;; privmsgs have replyfns
+            (let [reply (fn [modName msg]
+                          (doSomething [modName network (second recip) msg] nil))
+                  filter-fn (fn [mod-name]
+                              (let [my-recip (str network "#" (second recip))]
+                                (permits my-recip mod-name)))]
+              (hub/broadcast (str "irc " msg) reply filter-fn))
+            ;; otherwise don't
+            (hub/broadcast (str "irc " msg)))))
 
-      (when (and @registered (not-empty (:queue @conn)))
+      (when (not-empty (:queue @conn))
         (doseq [msg (:queue @conn)] (write conn msg))
         (dosync (alter conn assoc :queue [])))
       (Thread/sleep 100))
