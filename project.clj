6de0f8de045bd356eb1211636ac78baf4d921476
George Moomau <georgemoomau@gmail.com>
2013-03-21 22:52:53 -0400

    upgrade clojure and update conflig example

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject rekrvn "0.0.8-alpha"
   :description "irc bot framework"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [http.async.client "0.4.3"]
                  [cheshire "4.0.0"]
+                 [hickory "0.3.0"]
                  [org.clojars.sids/htmlcleaner "2.1"]
                  [com.novemberain/monger "1.4.1"]]
   :main rekrvn.hub)
