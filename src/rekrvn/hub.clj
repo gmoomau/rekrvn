02d36afe060337d61d5810ba2801932fc8690ed7
George Moomau <georgemoomau@gmail.com>
2013-03-21 21:25:28 -0700

    :reload-all is not the best idea

--- src/rekrvn/hub.clj
+++ src/rekrvn/hub.clj
@@ -18,18 +18,17 @@
 (defn addListener [modname matcher action]
   (dosync (alter listeners conj {:mod modname :matcher matcher :action action})))
 
-(defn modload [modId & [reload?]]
+(defn modload [modId]
   (let [module (symbol (str "rekrvn.modules." modId))]
     (try
-      (if reload?
-        (require module :reload-all)
-        (require module :reload))
+      (require module :reload)
       (catch Exception e (println (str "Caught exception: " (.getMessage e)))))))
 
 (defn reload [modId]
   (do
     (dosync (ref-set listeners (remove (fn [trig] (= modId (:mod trig))) @listeners)))
-    (modload modId true)))
+    (require 'rekrvn.config :reload)
+    (modload modId)))
 
 (defn initMods []
   (doseq [modName rekrvn.config/modules] (modload modName)))
