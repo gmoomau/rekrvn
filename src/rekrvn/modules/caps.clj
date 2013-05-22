642e2822748fcee9751cb8f163a9c2ead4f911e7
George Moomau <georgemoomau@gmail.com>
2013-05-21 20:44:56 -0700

    now you can unyell some caps

--- src/rekrvn/modules/caps.clj
+++ src/rekrvn/modules/caps.clj
@@ -5,15 +5,22 @@
 (def modName "caps")
 
 (defn caps [[channel line] reply]
-  (when (> (count line) 3)
+  (if-let [[_ term] (re-matches #"STOP YELLING (.+)" line)]
     (do
       (mongo/connect!)
-      (let [new-doc {:text line :channel channel}
-            finder {:channel channel}
-            result (mongo/get-rand-as-map modName finder)]
-        (mongo/insert modName new-doc)
-        (when result
-          (reply modName (:text result)))
-        (mongo/disconnect!)))))
+      (if (= 0 (.getN (mongo/remove modName {:channel channel :text term})))
+        (reply modName "I CAN'T!")
+        (reply modName "FINE."))
+      (mongo/disconnect!))
+    (when (> (count line) 3)
+      (do
+        (mongo/connect!)
+        (let [new-doc {:text line :channel channel}
+              finder {:channel channel}
+              result (mongo/get-rand-as-map modName finder)]
+          (mongo/insert modName new-doc)
+          (when result
+            (reply modName (:text result)))
+          (mongo/disconnect!))))))
 
-(hub/addListener modName #"^irc.*PRIVMSG #(\S+) :([^a-z]*[A-Z]+[^a-z]*)$" caps)
+  (hub/addListener modName #"^irc.*PRIVMSG #(\S+) :([^a-z]*[A-Z]+[^a-z]*)$" caps)
