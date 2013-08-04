5bedddc94c000456e4a2da859e49b7a87da926ab
Cole Brown <bigswim@gmail.com>
2013-08-04 19:18:54 -0400

    Moar bug fixes

--- src/rekrvn/modules/voveri/tests/engine_test.clj
+++ src/rekrvn/modules/voveri/tests/engine_test.clj
@@ -10,15 +10,32 @@
   (is (= (in-phase initial-game-state :inactive :success)
          :success)))
 
-(deftest join-test
+(deftest join-game-test
   (is (= (-> initial-game-state
              (join-game "cole")
              :players)
          {"cole" {:faction nil :vote nil :is-on-team nil}}))
   (is (= (-> initial-game-state
              (join-game "cole")
+             :phase)
+         :inactive))
+  (is (= (-> initial-game-state
+             (join-game "cole")
              (join-game "grog")
              {"cole" {:faction nil :vote nil :is-on-team nil}
               "grog" {:faction nil :vote nil :is-on-team nil}}))))
 
+(def five-player-game-state
+  (-> initial-game-state
+      (join-game "p1")
+      (join-game "p2")
+      (join-game "p3")
+      (join-game "p4")
+      (join-game "p5")))
 
+(def five-player-game-started-state
+  (start-game five-player-game-state))
+
+(deftest start-game-test
+  (is (= (:phase five-player-game-started-state)
+         :pick-team)))
