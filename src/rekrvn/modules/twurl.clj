ba52ded61fec7f65ba77444741be0e95febba909
George Moomau <georgemoomau@gmail.com>
2013-08-07 22:54:10 -0400

    use the twitter library

--- src/rekrvn/modules/twurl.clj
+++ src/rekrvn/modules/twurl.clj
@@ -1,5 +1,6 @@
 (ns rekrvn.modules.twurl
   (:require [rekrvn.hub :as hub])
+  (:require [rekrvn.modules.twitter :as util])
   (:use [cheshire.core])
   (:use [rekrvn.config :only [twitter-creds]])
   ;; consumer-key, consumer-token, user-token, user-secret
@@ -16,29 +17,12 @@
                                 (:consumer-secret twitter-creds)
                                 (:user-token twitter-creds)
                                 (:user-secret twitter-creds)))
-
-(defn expand-links [text urls]
-  (reduce #(clojure.string/replace %1 (:url %2) (:expanded_url %2)) text urls))
-
-(defn plaintext [text]
-  (-> text
-    (clojure.string/replace "&gt;" ">")
-    (clojure.string/replace "&lt;" "<")
-    (clojure.string/replace "&amp;" "&")))
-
-(defn bold [text] (when text (str (char 2) text (char 15))))
-;; 0x02 bolds in irc and 0x0F (decimal 15) removes formatting
-
-(defn niceify [tweet]
-  (when-let [user-string (bold (:screen_name (:user tweet)))]
-    (str user-string " " (expand-links (plaintext (:text tweet)) (:urls (:entities tweet))))))
-
 (defn get-tweet [id]
   (:body (statuses-show-id :oauth-creds my-creds
                            :params {:id id :include_entities true})))
 
 (defn twurl [[tweetid] reply]
-  (when-let [msg (niceify (get-tweet tweetid))]
+  (when-let [msg (util/niceify (get-tweet tweetid))]
     (reply mod-name msg)))
 
 (hub/addListener mod-name #"https?://\S*twitter\.com\S*/status(?:es)?/(\d+)" twurl)
