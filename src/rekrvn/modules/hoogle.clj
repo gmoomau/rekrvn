dc528df91d0d6e15aedf64090db9459484beb696
Carl Howells <chowells79@gmail.com>
2014-05-22 17:13:12 -0600

    Testing

--- src/rekrvn/modules/hoogle.clj
+++
@@ -1,7 +0,0 @@
-(ns rekrvn.modules.hoogle
-  (:require [rekrvn.hub :as hub]))
-
-(defn hoogle [[terms] reply]
-  (reply "hoogle" "A hoogle error has occurred."))
-
-(hub/addListener "hoogle" #"^irc.*PRIVMSG #\S+ :\.hoogle(\s.*)?$" hoogle)
