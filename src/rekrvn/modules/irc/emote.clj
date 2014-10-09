0e8e16931cab92a320d25e0c24a925ada834c108
George Moomau <georgemoomau@gmail.com>
2014-10-09 14:07:49 -0700

    Refactor emotes

--- src/rekrvn/modules/irc/emote.clj
+++ src/rekrvn/modules/irc/emote.clj
@@ -1,23 +1,18 @@
 (ns rekrvn.modules.irc.emote
-  (:require [rekrvn.hub :as hub]))
+  (:require [rekrvn.hub :as hub])
+  (:use [clojure.string :only [join]]))
 
 (def modName "irc.emote")
-(def shrug "¯\\_(ツ)_/¯")
-(def tup "d_(ツ)_d")
-(def tdn "q-(ツ)-q")
+(def faces {"shrug" "¯\\_(ツ)_/¯"
+            "tdn"   "q-(ツ)-q"
+            "tup"   "d-(ツ)-d"})
 
-(defn bubble [face message refn]
-  "speech bubbles if appropriate"
-  (if message
-    (do
-      (let [width (+ 2 (.length message))
-            line (apply str (repeat width "─"))]
-        (refn modName (apply str "     ┌" line "┐"))
-        (refn modName (apply str "     │ " message " │"))
-        (refn modName (apply str "     /" line "┘"))
-        (refn modName face)))
-    (refn modName face)))
+(defn say [[emote message] reply-fn]
+  (when message
+    (let [line (and message (-> message count inc (repeat "─") join))]
+      (reply-fn modName (str "     ┌" line "┐"))
+      (reply-fn modName (str "     │" message " │"))
+      (reply-fn modName (str "     /" line "┘"))))
+  (reply-fn modName (faces emote)))
 
-(hub/addListener modName #"^irc.*PRIVMSG \S+ :\.shrug(?: )?(.+)?" (fn [[msg] refn] (bubble shrug msg refn)))
-(hub/addListener modName #"^irc.*PRIVMSG \S+ :\.tup(?: )?(.+)?" (fn [[msg] refn] (bubble tup msg refn)))
-(hub/addListener modName #"^irc.*PRIVMSG \S+ :\.tdn(?: )?(.+)?" (fn [[msg] refn] (bubble tdn msg refn)))
+(hub/addListener modName #"^irc.*PRIVMSG \S+ :\.(shrug|tdn|tup)( .+)?$" say)
