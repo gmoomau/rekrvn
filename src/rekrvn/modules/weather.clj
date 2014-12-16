8ae53340e7e1eb84833a673b9d20e2736920e050
George Moomau <georgemoomau@gmail.com>
2014-12-16 00:15:34 -0800

    Persist .w locations and add .w @nick functionality

--- src/rekrvn/modules/weather.clj
+++ src/rekrvn/modules/weather.clj
@@ -1,6 +1,8 @@
 (ns rekrvn.modules.weather
-  (:require [rekrvn.hub :as hub])
-  (:require [http.async.client :as c])
+  (:require [rekrvn.hub :as hub]
+            [rekrvn.modules.mongo :as mongo]
+            [http.async.client :as c]
+            [clojure.string :as s])
   (:use [http.async.client.request :only [url-encode]]))
 
 (def mod-name "weather")
@@ -19,9 +21,37 @@
       (c/await response)
       (c/string response))))
 
-(defn weather [[location] reply]
-  (if-let [raw-weather (api-lookup location)]
-    (reply mod-name (niceify raw-weather))
-    (reply mod-name "This thing is really picky about locations. Try a zip code.")))
+(defn tell-weather [location reply]
+  (if location
+    ;; store the location and get weather
+    (if-let [raw-weather (api-lookup location)]
+      (reply mod-name (niceify raw-weather))
+      (reply mod-name (str "This thing is really picky about locations. Try a zip code or"
+                           " 3-letter airport code. Also .w is broken so maybe just give up.")))))
 
-(hub/addListener "weather" #"^irc.*PRIVMSG \S+ :\.w(?:eather)? (.+)$" weather)
+(defn location-for-nick [nick channel]
+  (:location (first (mongo/get-docs mod-name {:nick nick :channel channel}))))
+
+(defn weather [[speaker channel is-nick content] reply]
+  (if (or is-nick (not content))
+    ;; .w
+    ;; .w @NICK
+    (let [nick (s/lower-case (or content speaker))]
+      (do ;; get weather for that nick
+        (mongo/connect!)
+        (if-let [loc (location-for-nick nick channel)]
+          (tell-weather loc reply)
+          (reply mod-name (str "No location stored for " nick ".")))
+        (mongo/disconnect!)))
+    ;; .w PLACE
+    (let [nick (s/lower-case speaker)]
+      (do ;; store location for nick, get weather
+        (mongo/connect!)
+        (mongo/remove mod-name {:nick nick :channel channel})
+        (mongo/insert mod-name {:nick nick :channel channel :location content})
+        (mongo/disconnect!)
+        (tell-weather content reply)))))
+
+;; syntax: .w [PLACE | @NICK]
+(hub/addListener "weather" #"^irc :(\S+)!\S+ PRIVMSG (\S+) :\.w(?:eather)?(?: (@)?(.+)?)?$" weather)
+;;                            speaker^                 ^channel         is-nick^   ^content
