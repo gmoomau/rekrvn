300821e514151073a6bc653ae7e0bc0fabd9b91e
George Moomau <georgemoomau@gmail.com>
2015-06-08 09:11:55 -0700

    clojure version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rekrvn "0.1.0"
   :description "irc bot framework"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [http.async.client "0.4.3"]
                  [cheshire "4.0.0"]
                  [enlive "1.1.1"]
