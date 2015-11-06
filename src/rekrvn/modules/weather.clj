70413fba0d5c5f8056f46b1f83d9f778a690662a
George Moomau <georgemoomau@gmail.com>
2015-11-06 11:56:17 -0800

    add location check to weather.clj
    .w @location name
      checks weather for "location name" but does
      not alter the user's stored home

--- src/rekrvn/modules/weather.clj
+++ src/rekrvn/modules/weather.clj
@@ -50,7 +50,8 @@
         wind (:windSpeed now)]
     (str loc ": " summary " | " temp "°F (feels like " feels-like "°F) | " humidity "% humidity | wind " wind "mph")))
 
-(defn store-place [nick channel loc-info]
+(defn store-home [nick channel loc-info]
+  ;; saves home for nick/channel in db
   (mongo/connect!)
   (mongo/remove mod-name {:nick (s/lower-case nick) :channel channel})
   (mongo/insert mod-name {:nick (s/lower-case nick)
@@ -58,38 +59,44 @@
                           :loc loc-info})
   (mongo/disconnect!))
 
-(defn get-place [nick channel]
+(defn get-home [nick channel]
+  ;; checks db for hom stored for nick/channel
   (mongo/connect!)
   (let [place (first (mongo/get-docs mod-name {:nick (s/lower-case nick) :channel channel}))]
     (mongo/disconnect!)
     place))
 
-(defn forecast-for-nick [[speaker channel nick] reply]
-;; .w @nick
-  (if-let [place (:loc (get-place nick channel))]
-    (if-let [weather (get-weather place)]
-      (reply mod-name (make-forecast place weather))
-      (reply mod-name (str "Can't get weather for " nick)))
-    (reply mod-name (str "Can't find weather for " nick))))
+(defn check-forecast [[channel query] reply]
+  ;; .w @some string
+  ;; does not save anything to the db
+  ;; first check if the string is someone's nick. if it is find weather for them
+  ;; if not, treat it like a location and find weather
+  (let [place (or (:loc (get-home query channel)) (str-to-loc query))]
+    (if place
+      (when-let [weather (get-weather place)]
+        (reply mod-name (make-forecast place weather)))
+      (reply mod-name (str "Can't find weather for " query)))))
 
 (defn forecast-for-speaker [[nick channel] reply]
-;; .w
-  (forecast-for-nick [nick channel nick] reply))
+  ;; .w
+  (check-forecast [channel nick] reply))
 
 (defn forecast-for-location [[nick channel location] reply]
-;; .w location
+  ;; .w location
   (if-let [loc-info (str-to-loc location)]
     (if-let [weather (get-weather loc-info)]
       (do
-        (store-place nick channel loc-info)
+        (store-home nick channel loc-info)
         (reply mod-name (make-forecast loc-info weather)))
       (reply mod-name (str "Can't get weather for " (:name loc-info))))
     (reply mod-name (str "Can't find location: " location))))
 
+;; TODO: refactor because a lot of work is duplicated
+
 ;; .w
 (hub/addListener mod-name #"^irc :(\S+)!\S+ PRIVMSG (\S+) :\.w(?:eather)?\s*$" forecast-for-speaker)
-;; .w @nick
-(hub/addListener mod-name #"^irc :(\S+)!\S+ PRIVMSG (\S+) :\.w(?:eather)?\s+@(\S+)\s*$" forecast-for-nick)
+;; .w @something
+(hub/addListener mod-name #"^.*PRIVMSG (\S+) :\.w(?:eather)?\s+@(.*)$" check-forecast)
 ;; .w location
 (hub/addListener mod-name #"^irc :(\S+)!\S+ PRIVMSG (\S+) :\.w(?:eather)?\s+([^@].+)\s*$" forecast-for-location)
 
