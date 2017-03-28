54d17d020539c23609119d0954cf502de540db81
George Moomau <georgemoomau@gmail.com>
2017-03-28 06:08:42 -0700

    use new library for tweetstream
    new library handles tweet chunking and reconnect etc

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [cheshire "5.5.0"]
                  [enlive "1.1.6"]
                  [twitter-api "1.8.0"]
+                 [twitter-streaming-client "0.3.2"]
                  [http.async.client "1.2.0"]
                  [com.novemberain/monger "1.4.1"]]
   :main rekrvn.hub
--- src/rekrvn/modules/tweetstream.clj
+++ src/rekrvn/modules/tweetstream.clj
@@ -3,24 +3,18 @@
   (:use [cheshire.core])
   (:use [rekrvn.config :only [twitter-creds twitter-stream-channel]])
   (:require [rekrvn.modules.twitter :as util])
-  (:use [clojure.string :only [blank?]]
-        [twitter.oauth]
-        [twitter.callbacks]
-        [twitter.callbacks.handlers]
-        [twitter.api.streaming])
+  (:require [twitter-streaming-client.core :as twclient]
+            [twitter.oauth :as oauth])
+  (:use [clojure.string :only [blank?]])
   (:require [http.async.client :as ac])
-  (:import (twitter.callbacks.protocols AsyncStreamingCallback)
-           (java.lang Thread)))
+  (:import (java.lang Thread)))
 
 (def mod-name "tweetstream")
 
-(def my-creds (make-oauth-creds (:consumer-key twitter-creds)
+(def my-creds (oauth/make-oauth-creds (:consumer-key twitter-creds)
                                 (:consumer-secret twitter-creds)
                                 (:user-token twitter-creds)
                                 (:user-secret twitter-creds)))
-
-(declare open-user-stream)
-(def wait 30000)
 
 ;; TODO: multi-channel support
 (defn announce-tweet [tweet]
@@ -28,52 +22,15 @@
     (println "announcing tweet: " announcement)
     (hub/broadcast announcement)))
 
-;; temporary, get rid of this or something. (replace with proper logging?)
-(defn announce-error [message]
-  (let [error (str mod-name " forirc faceroar##dump " message)]
-    (println "announcing error: " error)
-    (hub/broadcast error)))
+(def stream (twclient/create-twitter-stream twitter.api.streaming/user-stream
+                                           :oauth-creds my-creds))
 
-(defn json-to-tweet [tweet-string]
-  (try
-    (parse-string tweet-string true)
-    (catch Exception e
-      (announce-error "partial tweet happened"))))
+(defn handle-tweets [stream]
+  (while true
+    (doseq [tweet (:tweet (twclient/retrieve-queues stream))]
+        (println "got tweet: " tweet)
+        (announce-tweet tweet))
+    (Thread/sleep 1000)))
 
-;;;;;;;;;;; a chain of functions for thread stuff
-(defn handle-part [response json-bytes]
-  ;; TODO: stop being terrible and aggregate chunks
-  ;; TODO: distinguish between different types of messages from twitter
-  (let [json (str json-bytes)]
-    (when (not (blank? json))
-      (println "from" mod-name json)
-      (announce-error "got something")
-      (when-let [tweet (json-to-tweet json)]
-        (announce-tweet tweet)))))
-
-(defn handle-failure [response]
-  (announce-error "twitter got mad"))
-;  (do
-;    (announce-error "twitter got mad")
-;    (Thread/sleep wait)
-;    (open-user-stream)))
-
-(defn handle-exception [response throwable]
-  (announce-error (str "exception " throwable)))
-;  (do
-;    (announce-error (str "exception " throwable))
-;    (println throwable)
-;    (Thread/sleep wait)
-;    (open-user-stream)))
-
-;; twitter response callback
-(def ^:dynamic *custom-streaming-callback*
-  (AsyncStreamingCallback. ;; on-bodypart, on-failure, on-exception
-    handle-part
-    handle-failure
-    handle-exception))
-
-(defn open-user-stream []
-  (user-stream :oauth-creds my-creds :callbacks *custom-streaming-callback*))
-
-(open-user-stream)
+(twclient/start-twitter-stream stream)
+(doto (Thread. #(handle-tweets stream)) (.start))
