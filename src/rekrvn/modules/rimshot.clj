9e0017b47852ed19f4524d3932823d9ec5a042e4
George Moomau <georgemoomau@gmail.com>
2017-03-28 03:23:56 -0700

    add more reactions to .rimshot

--- src/rekrvn/modules/rimshot.clj
+++ src/rekrvn/modules/rimshot.clj
@@ -8,9 +8,13 @@
 (def trombone "http://sadtrombone.com")
 (def khan "http://www.khaaan.com/")
 (def snowman "☃")
+(def tableflip "(╯°□°)╯︵ ┻━┻")
+(def surf "🏄")
 
 (hub/addListener modName #"^irc.*PRIVMSG \S+ :(\.(?:instant)?rimshot)$" (fn [_ refn] (refn modName rimShot)))
 (hub/addListener modName #"^irc.*PRIVMSG \S+ :(\.(?:sad)?trombone)$" (fn [_ refn] (refn modName trombone)))
 (hub/addListener modName #"(?i)^irc.*PRIVMSG \S+ :(\.kha+n)$" (fn [_ refn] (refn modName khan)))
 (hub/addListener modName #"(?i)^irc.*PRIVMSG \S+ :(\.snowman)$" (fn [_ refn] (refn modName snowman)))
+(hub/addListener modName #"(?i)^irc.*PRIVMSG \S+ :(\.flip)$" (fn [_ refn] (refn modName tableflip)))
+(hub/addListener modName #"(?i)^irc.*PRIVMSG \S+ :(\.surf)$" (fn [_ refn] (refn modName surf)))
 
