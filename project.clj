84a716a24763c52aeb302116b9d931c3f823953f
George Moomau <georgemoomau@gmail.com>
2017-03-28 03:30:57 -0700

    update library versions

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject rekrvn "0.1.0"
   :description "irc bot framework"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http.async.client "0.4.3"]
-                 [cheshire "4.0.0"]
-                 [enlive "1.1.1"]
-                 [twitter-api "0.7.5"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.5.0"]
+                 [enlive "1.1.6"]
+                 [twitter-api "1.8.0"]
+                 [http.async.client "0.5.1"]
                  [com.novemberain/monger "1.4.1"]]
   :main rekrvn.hub
   :test-paths ["test"])
