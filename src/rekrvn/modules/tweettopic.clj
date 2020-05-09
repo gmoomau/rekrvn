1cac35714baf9c845cbd116b6be88bebf127f5e2
George Moomau <georgemoomau@gmail.com>
2020-05-08 22:08:15 -0700

    twitter cleanup
    refactor twitter modules
    clear unused dependencies
    remove obsolete tweetstream

--- src/rekrvn/modules/tweettopic.clj
+++ src/rekrvn/modules/tweettopic.clj
@@ -1,21 +1,16 @@
 (ns rekrvn.modules.tweettopic
-  (:require [rekrvn.config :refer [twitter-creds]]
+  (:require [rekrvn.config :refer [tweet-topic-channel twitter-creds]]
             [rekrvn.hub :as hub]
-            [twitter.api.restful :refer :all]
-            [twitter.oauth :refer :all]))
+            [rekrvn.modules.twitter :as util]
+            [twitter.api.restful :refer :all]))
 
 (def mod-name "tweettopic")
-
-(def my-creds (make-oauth-creds (:consumer-key twitter-creds)
-                                (:consumer-secret twitter-creds)
-                                (:user-token twitter-creds)
-                                (:user-secret twitter-creds)))
+;; docs at https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update .
 
 (defn tweet-topic [[channel text] & col]
-  (let [truncated-text (if (> (.length text) 140) (str (subs text 0 135) "...") text)]
-    (if (= channel "#room")
-      (statuses-update :oauth-creds my-creds
-                       :params {:status truncated-text}))))
+  (if (= channel tweet-topic-channel)
+    (let [truncated-text (if (> (.length text) 280) (str (subs text 0 275) "...") text)]
+      (statuses-update :oauth-creds util/my-creds :params {:status truncated-text}))))
 
 (hub/addListener mod-name #"irc \S+ TOPIC (\S+) :(.+\S.*)$" tweet-topic)
 
