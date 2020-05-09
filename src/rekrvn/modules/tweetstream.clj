8418537595f6a6ba9e3c6315bc8e896dc4dfe460
George Moomau <georgemoomau@gmail.com>
2020-05-09 13:29:57 -0700

    WIP: bring back tweetstream
    mostly works. still need to handle errors and remembering last

--- src/rekrvn/modules/tweetstream.clj
+++ src/rekrvn/modules/tweetstream.clj
@@ -1,6 +1,5 @@
 (ns rekrvn.modules.tweetstream
-  (:require [rekrvn.config :refer [twitter-creds
-                                   twitter-stream-channel]]
+  (:require [rekrvn.config :refer [twitter-stream-channel]]
             [rekrvn.hub :as hub]
             [rekrvn.modules.twitter :as util]
             [twitter.api.restful :refer :all]
@@ -8,6 +7,7 @@
             [clojure.tools.logging :as log]))
 
 (def mod-name "tweetstream")
+; docs at https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-home_timeline .
 ;; WIP
 
 (defn announce-tweet [tweet]
@@ -16,3 +16,30 @@
     (hub/broadcast announcement))
   (when-let [quoted (:quoted_status tweet)]
     (hub/broadcast (str mod-name " forirc " twitter-stream-channel " " (util/niceify quoted)))))
+
+(defn st []
+  ;; just for testing, delete.
+  (statuses-home-timeline :oauth-creds util/my-creds
+                          :params {:since_id 1258952255698546694
+                                   :include_entities true}))
+
+;; TODO handle "Twitter responded to request with error 88: Rate limit exceeded. Next reset at 1589021843 (UTC epoch seconds)"
+;; wait til then
+;; log an error
+;; alert?
+;;
+(defn stream []
+  (loop [last-seen 1258952255698546694] ; TODO actually store and remember this
+    (log/info "last seen: " last-seen)
+    (let [tweets (:body (statuses-home-timeline
+                              :oauth-creds util/my-creds
+                              :params {:since_id last-seen :include_entities true}))
+          new-last (or (-> tweets first :id) last-seen)]
+          ; ^ assumes the list is reverse chronological order
+          ;   the (or ..) is in case tweets is empty or something
+      (log/info "first tweet: " (-> tweets first :text))
+      (doseq [_ tweets] (announce-tweet _))
+      (Thread/sleep (* 70 1000)) ;; must be above 60sec. limit is 15req/15min
+      (recur new-last))))
+
+(doto (Thread. #(stream)) (.start))
