4b85a4d6040163c7a7f9293b745dab54e1a1972b
George Moomau <georgemoomau@gmail.com>
2020-05-24 17:43:22 -0700

    handle tweetstream on startup
    just grab the 3 latest tweets on startup to bootstrap tweetstream

--- src/rekrvn/modules/tweetstream.clj
+++ src/rekrvn/modules/tweetstream.clj
@@ -3,12 +3,10 @@
             [rekrvn.hub :as hub]
             [rekrvn.modules.twitter :as util]
             [twitter.api.restful :refer :all]
-            [twitter.oauth :as oauth]
             [clojure.tools.logging :as log]))
 
 (def mod-name "tweetstream")
-; docs at https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-home_timeline .
-;; WIP
+; docs https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-home_timeline .
 
 (defn announce-tweet [tweet]
   (let [announcement (str mod-name " forirc " twitter-stream-channel " " (util/niceify tweet))]
@@ -17,29 +15,31 @@
   (when-let [quoted (:quoted_status tweet)]
     (hub/broadcast (str mod-name " forirc " twitter-stream-channel " " (util/niceify quoted)))))
 
-(defn st []
-  ;; just for testing, delete.
-  (statuses-home-timeline :oauth-creds util/my-creds
-                          :params {:since_id 1258952255698546694
-                                   :include_entities true}))
+(defn get-statuses [last-seen]
+  (try
+    (statuses-home-timeline
+      :oauth-creds util/my-creds
+      :params (merge {:include_entities true :tweet_mode "extended"} last-seen))
+    (catch Exception e
+      (log/error "tweetstream exception: " e)
+      nil)))
 
 ;; TODO handle "Twitter responded to request with error 88: Rate limit exceeded. Next reset at 1589021843 (UTC epoch seconds)"
-;; wait til then
-;; log an error
-;; alert?
-;;
+;; wait til then, log an error, alert somewhere
+
 (defn stream []
-  (loop [last-seen 1258952255698546694] ; TODO actually store and remember this
+  (loop [last-seen {:count 3}] ; the first time through there's no last seen, so just grab 3 tweets
     (log/info "last seen: " last-seen)
-    (let [tweets (:body (statuses-home-timeline
-                              :oauth-creds util/my-creds
-                              :params {:since_id last-seen :include_entities true :tweet_mode "extended"}))
-          new-last (or (-> tweets first :id) last-seen)]
-          ; ^ assumes the list is reverse chronological order
-          ;   the (or ..) is in case tweets is empty or something
-      (log/info "first tweet: " (-> tweets first :text))
-      (doseq [_ tweets] (announce-tweet _))
-      (Thread/sleep (* 70 1000)) ;; must be above 60sec. limit is 15req/15min
-      (recur new-last))))
+    (if-let [raw-statuses (get-statuses last-seen)]
+      (let [tweets (:body raw-statuses)
+            newest-seen (-> tweets first :id)
+            new-last (if newest-seen {:since_id newest-seen} last-seen)]
+        (log/info "first tweet: " (-> tweets first :text))
+        (doseq [_ (reverse tweets)] (announce-tweet _))
+        (Thread/sleep (* 70 1000)) ;; must be above 60sec. limit is 15req/15min
+        (recur new-last))
+      (do
+        (Thread/sleep (* 300 1000)) ;; sleep 5min if there was an error
+        (recur last-seen)))))
 
 (doto (Thread. #(stream)) (.start))
